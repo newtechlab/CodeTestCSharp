134d92c3b855c9d49da97af813d27bfb6aff6b0c
Espen Sande Larsen <workingclasshacker@gmail.com>
2018-05-14 13:48:09 +0200

    Update Program.cs

--- CodeTestCSharp/CodeTest/Program.cs
+++ CodeTestCSharp/CodeTest/Program.cs
@@ -8,7 +8,7 @@ namespace CodeTest
         {
             //task make this compile, run and output the first 10 fibonacci numbers, separated by a comma and a space :)
 
-            Console.WriteLine($"{0 < fib > 10 < join > ", "}"); //print 1, 1, 2, 3, ....
+            Console.WriteLine($"{0 <fib> 10 <join> ", "}"); //print 1, 1, 2, 3, ....
             Console.Read();
         }
     }
