8e5b2350ab0589900d78a3fdfa7d0742c0f3c98f
= <=>
2018-05-14 13:47:31 +0200

    may the force be with you...

---
+++ CodeTestCSharp/CodeTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CodeTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            //task make this compile, run and output the first 10 fibonacci numbers, separated by a comma and a space :)
+
+            Console.WriteLine($"{0 < fib > 10 < join > ", "}"); //print 1, 1, 2, 3, ....
+            Console.Read();
+        }
+    }
+}
