fb888e6cdb9cb82b20c218051b45b6e53dd31dd1
Espen Sande Larsen <workingclasshacker@gmail.com>
2018-05-14 13:49:57 +0200

    may the force be with you...

--- CodeTestCSharp/CodeTest/Program.cs
+++ CodeTestCSharp/CodeTest/Program.cs
@@ -6,7 +6,7 @@ namespace CodeTest
     {
         static void Main()
         {
-            //task make this compile, run and output the first 10 fibonacci numbers, separated by a comma and a space :)
+            //task: make this compile, run and output the first 10 fibonacci numbers, separated by a comma and a space :)
             //may the force be with you...
             Console.WriteLine($"{0 <fib> 10 <join> ", "}"); //print 1, 1, 2, 3, ....
             Console.Read();
